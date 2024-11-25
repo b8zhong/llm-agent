8395c2238e0ddbf51bdaccfb7e30b4a1b4177149
Brayden Zhong <b8zhong@uwaterloo.ca>
2024-11-24 22:27:55 -0500

    Fixed memory interaction

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
 import 'dotenv/config'
 import {runLLM} from './src/llm'
+import {getMessages} from './src/memory'
 
 const userMessage = process.argv[2]
 
 if (!userMessage) {
-  console.error('Please provide a message')
-  process.exit(1)
+	console.error('Please provide a message')
+	process.exit(1)
 }
 
+const messages = await getMessages()
 const response = await runLLM({
-  messages: [{role: 'user', content: userMessage}]
+	messages: [
+		...messages,
+		{role: 'user', content: userMessage}
+	]
 })
 
 console.log(response)
--- src/memory.ts
+++ src/memory.ts
@@ -2,12 +2,12 @@ import { JSONFilePreset } from 'lowdb/node'
 import type { AIMessage } from '../types'
 import { v4 as uuidv4 } from 'uuid'
 
-export type MessageWithMetadata = AIMessage & { // Custom type
+export type MessageWithMetadata = AIMessage & {
   id: string
   createdAt: string
 }
 
-export const addMetadata = (message: AIMessage): MessageWithMetadata => ({ // Date, time and ID
+export const addMetadata = (message: AIMessage): MessageWithMetadata => ({
   ...message,
   id: uuidv4(),
   createdAt: new Date().toISOString(),
@@ -40,4 +40,3 @@ export const getMessages = async () => {
   const db = await getDb()
   return db.data.messages.map(removeMetadata)
 }
-
