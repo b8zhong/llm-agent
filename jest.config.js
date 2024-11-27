8e05c05f74bce76717c29d65c398b69a46ca7ef8
Brayden Zhong <b8zhong@uwaterloo.ca>
2024-11-26 22:56:46 -0500

    Fixed ESM only modules

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
+  },
+  extensionsToTreatAsEsm: ['.ts']
 };
